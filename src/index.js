8987b19214208fb51c15db9b4763685f6678292a
Nathaniel Colon <nathanielcolon12@gmail.com>
2024-01-10 15:04:53 -0500

    Used FF to make weather data obj

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
+//Getting JSON data from Weather API using await fetch
 async function getWeather(location) {
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=e3a2a8b3e8d34b518f0225440240901&q=${location}`,
     { mode: "cors" }
   );
   const weatherData = await response.json();
-  console.log(weatherData.current.feelslike_f);
+  const weatherDataObject = returnWeatherDataObject(weatherData);
+  console.log(weatherDataObject.weatherCondition);
 }
 
-function returnWeatherDataObject(jsonObject) {}
+//Using Factory Function to return weather Data object from JSON
+function returnWeatherDataObject(jsonObject) {
+  const city = jsonObject.location.name;
+  const country = jsonObject.location.name;
+  const weatherCondition = jsonObject.current.condition.text;
+  const tempF = jsonObject.current.temp_f;
+  const tempC = jsonObject.current.temp_c;
+  const feelsLikeF = jsonObject.current.feelslike_f;
+  const feelsLikeC = jsonObject.current.feelslike_c;
+  const windMPH = jsonObject.current.wind_mph;
+  const windKPH = jsonObject.current.wind_kph;
+  const humidity = jsonObject.current.humidity;
+  const UV = jsonObject.current.uv;
+
+  return {
+    city,
+    country,
+    weatherCondition,
+    tempF,
+    tempC,
+    feelsLikeF,
+    feelsLikeC,
+    windMPH,
+    windKPH,
+    humidity,
+    UV,
+  };
+}
+
 getWeather("New York");
