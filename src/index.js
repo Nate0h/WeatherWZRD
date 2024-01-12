3039d206a309d234e68ff9d2f9f95ee7274a1250
Nathaniel Colon <nathanielcolon12@gmail.com>
2024-01-12 16:57:31 -0500

    Added fahrenheit to celsius button, vv

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const form = document.getElementById("form");
 const input = document.getElementById("location");
-
+const switchTemp = document.getElementById("switch");
 const city = document.querySelector(".city");
 const country = document.querySelector(".country");
 const time = document.querySelector(".time");
-const temperatureFahr = document.querySelector(".temperature .temp-data");
+const temperature = document.querySelector(".temperature .temp-data");
 const weatherState = document.querySelector(".state");
-const temperatureFeelsFahr = document.querySelector(
+const temperatureFeelsLike = document.querySelector(
   ".weather-descr .temp-data"
 );
+let unit = "°F";
+
+const tempUnit = document.querySelectorAll(".temp-unit");
 const weatherIcon = document.querySelector(".weather-icon");
 const wind = document.querySelector(".wind");
 const humidity = document.querySelector(".humidity");
@@ -17,12 +20,12 @@ const UVIndex = document.querySelector(".uv-index");
 //Listen to when form is submitted and calls getWeather function
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  getWeather(input.value);
-  input.value = "";
+  getWeather(input.value, unit);
+  form.reset();
 });
 
 //Getting JSON data from Weather API using await fetch
-async function getWeather(location) {
+async function getWeather(location, unit) {
   try {
     const response = await fetch(
       `https://api.weatherapi.com/v1/current.json?key=e3a2a8b3e8d34b518f0225440240901&q=${location}`,
@@ -30,8 +33,7 @@ async function getWeather(location) {
     );
     const weatherData = await response.json();
     const weatherDataObject = returnWeatherDataObject(weatherData);
-    populateUI(weatherDataObject);
-    console.log(weatherData);
+    populateUI(weatherDataObject, unit);
   } catch (error) {
     alert(
       "Location Not Found.\nPlease make sure search is a valid Zipcode, City, State or Country"
@@ -70,15 +72,31 @@ function returnWeatherDataObject(jsonObject) {
   };
 }
 
-function populateUI(weatherDataObject) {
+function populateUI(weatherDataObject, unit) {
+  if (unit === "°F") {
+    temperature.textContent = weatherDataObject.tempF;
+    temperatureFeelsLike.textContent = weatherDataObject.feelsLikeF;
+    tempUnit[0].textContent = unit;
+    tempUnit[1].textContent = unit;
+  } else {
+    temperature.textContent = weatherDataObject.tempC;
+    temperatureFeelsLike.textContent = weatherDataObject.feelsLikeC;
+    tempUnit[0].textContent = unit;
+    tempUnit[1].textContent = unit;
+  }
   city.textContent = weatherDataObject.city;
   country.textContent = weatherDataObject.country;
   time.textContent = weatherDataObject.time;
-  temperatureFahr.textContent = weatherDataObject.tempF;
-  temperatureFeelsFahr.textContent = weatherDataObject.feelsLikeF;
   weatherState.textContent = weatherDataObject.weatherCondition;
   wind.textContent = weatherDataObject.wind;
   humidity.textContent = weatherDataObject.humidity;
   UVIndex.textContent = weatherDataObject.uv;
   weatherIcon.src = weatherDataObject.icon;
 }
+
+switchTemp.addEventListener("click", (e) => {
+  unit = unit === "°F" ? "°C" : "°F";
+  getWeather(city.textContent, unit);
+  e.stopPropagation();
+  e.preventDefault();
+});
